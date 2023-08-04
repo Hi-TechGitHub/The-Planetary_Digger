5ca8dc1def9901abda6f767f5845f251bccded9e
HiTechGitHub <vgorbanev702@gmail.com>
2023-08-05 02:00:15 +0300

    Send game in Github master

---
+++ src/main/java/org/Game/Mechanics/mechanic.java
@@ -0,0 +1,47 @@
+package org.Game.Mechanics;
+
+import org.Game.Resources.EarthRes;
+
+import java.util.Random;
+
+public class Mechanic {
+    public void randomIncrement(EarthRes earthRes) {
+
+        Random random = new Random();
+
+        // dirt с 20% шансом
+        if (random.nextInt(100) < 20) {
+            earthRes.setDirt(earthRes.getDirt() + 1);
+        }
+
+        // wood с 10% шансом
+        if (random.nextInt(100) < 10) {
+            earthRes.setWood(earthRes.getWood() + 1);
+        }
+
+        // preciousStones с 5% шансом
+        if (random.nextInt(100) < 5) {
+            earthRes.setPreciousStones(earthRes.getPreciousStones() + 1);
+        }
+
+        // oil с 15% шансом
+        if (random.nextInt(100) < 15) {
+            earthRes.setOil(earthRes.getOil() + 1);
+        }
+
+        // water с 20% шансом
+        if (random.nextInt(100) < 20) {
+            earthRes.setWater(earthRes.getWater() + 1);
+        }
+
+        // stone с 20% шансом
+        if (random.nextInt(100) < 20) {
+            earthRes.setStone(earthRes.getStone() + 1);
+        }
+
+        // gases с 10% шансом
+        if (random.nextInt(100) < 10) {
+            earthRes.setGases(earthRes.getGases() + 1);
+        }
+    }
+}
